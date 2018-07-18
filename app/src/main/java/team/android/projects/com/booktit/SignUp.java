9eb23fb3ffabb27102917287fcbc7aa42aa82384
Woo Jia Hao <woojiahao1234@gmail.com>
2018-07-18 17:11:12 +0800

    Connected the sign up page to let it navigate to and recognise other inputs

--- app/src/main/java/team/android/projects/com/booktit/SignUp.java
+++ app/src/main/java/team/android/projects/com/booktit/SignUp.java
@@ -1,13 +1,46 @@
 package team.android.projects.com.booktit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
+	private Button mSignUpBtn;
+	private Button mSignInBtn;
+	private EditText mEmailField;
+	private EditText mUsernameField;
+	private EditText mPasswordField;
 	
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sign_up);
+		
+		init();
+		connectListeners();
+	}
+	
+	private void init () {
+		mSignUpBtn = findViewById(R.id.signUpBtn);
+		mSignInBtn = findViewById(R.id.switchSignInBtn);
+		
+		mEmailField = findViewById(R.id.signupEmailField);
+		mUsernameField = findViewById(R.id.signupUsernameField);
+		mPasswordField = findViewById(R.id.signupPasswordField);
+	}
+	
+	private void connectListeners () {
+		mSignUpBtn.setOnClickListener(ev -> {
+			String enteredEmail = mEmailField.getText().toString();
+			String enteredUsername = mUsernameField.getText().toString();
+			String enteredPassword = mPasswordField.getText().toString();
+			
+			Toast.makeText(this, String.format("Email: %s, Username: %s, Password: %s", enteredEmail, enteredUsername, enteredPassword), Toast.LENGTH_SHORT).show();
+		});
+		
+		mSignInBtn.setOnClickListener(ev -> startActivity(new Intent(this, SignIn.class)));
 	}
 }
