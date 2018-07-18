13871b2f055a6166decb9b8f9d62c4078adf758b
Woo Jia Hao <woojiahao1234@gmail.com>
2018-07-18 16:53:17 +0800

    Added empty activity for SignIn screen

---
+++ app/src/main/java/team/android/projects/com/booktit/SignIn.java
@@ -0,0 +1,13 @@
+package team.android.projects.com.booktit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sign_in);
+	}
+}
