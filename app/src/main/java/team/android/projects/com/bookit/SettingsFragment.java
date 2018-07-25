13ac36e368f35a07086f78fa3fb3b88613b2e4f8
Woo Jia Hao <woojiahao1234@gmail.com>
2018-07-25 18:33:55 +0800

    Completed the design of a settings row

--- app/src/main/java/team/android/projects/com/bookit/SettingsFragment.java
+++ app/src/main/java/team/android/projects/com/bookit/SettingsFragment.java
@@ -1,6 +1,7 @@
 package team.android.projects.com.bookit;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,7 @@ public class SettingsFragment extends Fragment {
 	private View mView;
 	
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		mView = inflater.inflate(R.layout.fragment_settings, container, false);
 		init();
---
+++ app/src/main/java/team/android/projects/com/bookit/utils/ui/custom_views/settings_row/SettingsRow.java
@@ -0,0 +1,20 @@
+package team.android.projects.com.bookit.utils.ui.custom_views.settings_row;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class SettingsRow extends LinearLayout {
+	public SettingsRow(Context context) {
+		super(context);
+	}
+	
+	public SettingsRow(Context context, @Nullable AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+	public SettingsRow(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+}
