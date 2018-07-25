de7e9ab04178243ed25d4c3b9073f7846459362f
Woo Jia Hao <woojiahao1234@gmail.com>
2018-07-26 07:24:48 +0800

    Completed the init and connectListeners for favourites fragment

--- app/src/main/java/team/android/projects/com/bookit/FavouritesFragment.java
+++ app/src/main/java/team/android/projects/com/bookit/FavouritesFragment.java
@@ -1,25 +1,42 @@
 package team.android.projects.com.bookit;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import team.android.projects.com.bookit.utils.ui.custom_views.clearable_edit_text.ClearableEditText;
+
+import static team.android.projects.com.bookit.utils.ui.UIUtils.find;
 
 public class FavouritesFragment extends Fragment {
 	private View mView;
+	private ClearableEditText mFavouritesSearch;
+	private RecyclerView mFavouritesList;
 	
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		mView = inflater.inflate(R.layout.fragment_favourites, container, false);
 		
 		init();
+		connectListeners();
 		
 		return mView;
 	}
 	
 	private void init() {
+		mFavouritesSearch = find(mView, R.id.favouritesSearch);
+		mFavouritesList = find(mView, R.id.favouritesList);
+	}
 	
+	private void connectListeners() {
+		mFavouritesSearch.setOnTypingListener((sequence, start, before, count) -> {
+			Toast.makeText(getContext(), "Typing: " + sequence, Toast.LENGTH_SHORT).show();
+		});
 	}
 }
