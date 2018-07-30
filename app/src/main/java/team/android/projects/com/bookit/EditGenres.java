006cdd30b19a558ee827e025eb63cbc09b7a7816
Woo Jia Hao <woojiahao1234@gmail.com>
2018-07-30 10:07:50 +0800

    Added the edit genre screen

---
+++ app/src/main/java/team/android/projects/com/bookit/EditGenres.java
@@ -0,0 +1,13 @@
+package team.android.projects.com.bookit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditGenres extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_genres);
+	}
+}
