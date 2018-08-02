dbb7e0e5db1744eacd6cc29f3b1fe1f94cb4ef94
Woo Jia Hao <woojiahao1234@gmail.com>
2018-08-02 18:43:12 +0800

    completed the high-res image taking

--- app/src/main/java/team/android/projects/com/bookit/ScannerLauncher.java
+++ app/src/main/java/team/android/projects/com/bookit/ScannerLauncher.java
@@ -2,17 +2,29 @@ package team.android.projects.com.bookit;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import team.android.projects.com.bookit.utils.logging.ApplicationCodes;
 
 // todo: replace the processing method with an actual processing method
 // todo: make the book object parcelable so as to be able to pass it into the bundle as a key-value pair
 // todo: pass the book object in the bundle instead of just the title
 public class ScannerLauncher extends AppCompatActivity {
 	private final int REQUEST_IMAGE_CAPTURE = 2033;
+	private String mImageFilePath;
+	private Uri mPhotoURI;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +35,10 @@ public class ScannerLauncher extends AppCompatActivity {
 	}
 	
 	@Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == REQUEST_IMAGE_CAPTURE) {
+		if (requestCode == REQUEST_IMAGE_CAPTURE && mImageFilePath != null) {
 			switch (resultCode) {
 				case RESULT_OK:
-					Bundle extras = data.getExtras();
-					if (extras != null) {
-						processImage((Bitmap) extras.get("data"));
-					} else {
-						Toast.makeText(this, "Unable to load a preview of the image!", Toast.LENGTH_SHORT).show();
-					}
+					processImage(mPhotoURI);
 					break;
 				case RESULT_CANCELED:
 					finish();
@@ -43,20 +50,43 @@ public class ScannerLauncher extends AppCompatActivity {
 	private void init() {
 		Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		if (cameraIntent.resolveActivity(getPackageManager()) != null) {
-			startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+			File photo = null;
+			try {
+				photo = createImageFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+				Log.e(ApplicationCodes.Error.name(), "Error when creating image file");
+				// todo: exit the camera space
+			}
+			
+			if (photo != null) {
+				mPhotoURI = FileProvider.getUriForFile(this,
+						"com.example.android.fileprovider",
+						photo);
+				cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mPhotoURI);
+				startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+			}
 		}
 	}
 	
-	/**
-	 * Process the image and returns an appropriate result back to the calling activity
-	 *
-	 * @param target Bitmap to check
-	 */
-	private void processImage(Bitmap target) {
+	private void processImage(Uri imageUri) {
 		Intent i = new Intent();
 		i.putExtra("hasMatch", false); // placeholder of true
 		i.putExtra("extractedText", "Hello world"); // placeholder of Hello world
 		setResult(Activity.RESULT_OK, i);
 		finish();
+	}
+	
+	private File createImageFile() throws IOException {
+		String timeStamp = new SimpleDateFormat(
+				"yyyyMMdd_HHmmss",
+				Locale.getDefault())
+				.format(new Date());
+		String imageFileName = "JPEG_" + timeStamp + "_";
+		File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+		File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+		
+		mImageFilePath = image.getAbsolutePath();
+		return image;
 	}
 }
