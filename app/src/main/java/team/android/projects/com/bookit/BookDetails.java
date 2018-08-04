d6e9dc9e7bc3e54998044117314911b6284efb80
Woo Jia Hao <woojiahao1234@gmail.com>
2018-08-04 14:30:42 +0800

    Removed unnecessary instance variables for BookDetails

--- app/src/main/java/team/android/projects/com/bookit/BookDetails.java
+++ app/src/main/java/team/android/projects/com/bookit/BookDetails.java
@@ -23,9 +23,6 @@ public class BookDetails extends AppCompatActivity {
 	private ImageView mBackBtn;
 	private ImageView mFavouriteBtn;
 	
-	private TextView mBookISBN;
-	private TextView mBookPrice;
-	
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
